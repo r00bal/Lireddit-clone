ef20e00404ef46338137172d02fc901f8d709c9c
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-10-01 15:53:09 +0200

    add forgot password resolver

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,10 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolvers } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
-import redis from 'redis';
+import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
-import { User } from './entities/User';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -24,7 +23,7 @@ const main = async () => {
   const app = express();
 
   const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
+  const redis = new Redis();
   app.use(
     cors({
       origin: 'http://localhost:3000',
@@ -35,7 +34,7 @@ const main = async () => {
     session({
       name: COOKIE_NAME,
       store: new RedisStore({
-        client: redisClient,
+        client: redis,
         disableTouch: true,
       }),
       cookie: {
@@ -55,7 +54,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolvers, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
   });
 
   apolloServer.applyMiddleware({
