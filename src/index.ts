71c4bdd25ce706438d27bc717d1365f1dbc6262f
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-10-02 12:43:01 +0200

    switch to type orm

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
-import { MikroORM } from '@mikro-orm/core';
+
 import { __prod__, COOKIE_NAME } from './constants';
-import microConfig from './mikro-orm.config';
+
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
@@ -12,13 +12,20 @@ import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
+import { Post } from './entities/Post';
+import { User } from './entities/User';
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
-
-  // await orm.em.nativeDelete(User, {});
-
-  await orm.getMigrator().up();
+  const conn = createConnection({
+    type: 'postgres',
+    database: 'lireddit2',
+    username: 'postgres',
+    password: 'postgres',
+    logging: true,
+    synchronize: true,
+    entities: [Post, User],
+  });
 
   const app = express();
 
@@ -54,7 +61,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolvers, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
+    context: ({ req, res }) => ({ req, res, redis }),
   });
 
   apolloServer.applyMiddleware({
