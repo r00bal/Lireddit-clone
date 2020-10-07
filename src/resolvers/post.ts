d2a11b7dd021038956815cbab218bae2e9eda812
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-10-07 14:16:47 +0200

    modify posts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -12,6 +12,7 @@ import {
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
 import { isAuth } from '../middleware/isAuth';
+import { getConnection } from 'typeorm';
 
 @InputType()
 class PostInput {
@@ -24,8 +25,21 @@ class PostInput {
 @Resolver()
 export class PostResolvers {
   @Query(() => [Post])
-  posts(): Promise<Post[]> {
-    return Post.find();
+  async posts(
+    @Arg('limit', () => Int) limit: number,
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(50, limit);
+    const gb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder('p')
+      .orderBy('"createdAt"', 'DESC')
+      .take(realLimit);
+
+    if (cursor) {
+      gb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) });
+    }
+    return gb.getMany();
   }
 
   @Query(() => Post, { nullable: true })
