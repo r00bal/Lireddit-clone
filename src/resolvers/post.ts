b11124dc56f6ed1eb2081799f722ac19c6dae465
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-10-15 15:33:13 +0200

    Change Vote

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -110,7 +110,8 @@ export class PostResolvers {
   @Query(() => PaginatedPosts)
   async posts(
     @Arg('limit', () => Int) limit: number,
-    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null,
+    @Ctx() { req }: MyContext
   ): Promise<PaginatedPosts> {
     // 20 -> 21
     const realLimit = Math.min(50, limit);
@@ -118,8 +119,14 @@ export class PostResolvers {
 
     const replacments: any[] = [realLimitPlusOne];
 
+    if (req.session.userId) {
+      replacments.push(req.session.userId);
+    }
+
+    let cursorIdx = 3;
     if (cursor) {
       replacments.push(new Date(parseInt(cursor)));
+      cursorIdx = replacments.length;
     }
 
     const posts = await getConnection().query(
@@ -131,10 +138,15 @@ export class PostResolvers {
       'email', u.email,
       'createdAt', u."createdAt",
       'updatedAt', u."updatedAt"
-      ) creator
+      ) creator,
+     ${
+       req.session.userId
+         ? '(select value from updoot where "userId" = $2 and "postId" = p.id) "voteStatus"'
+         : 'null as "voteStatus"'
+     }
     from post p
     inner join public.user u on u.id = p."creatorId"
-    ${cursor ? `where p."createdAt" < $2` : ''}
+    ${cursor ? `where p."createdAt" < $${cursorIdx}` : ''}
     order by p."createdAt" DESC
     limit $1
     `,
